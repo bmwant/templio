8e4c00a022b479dae2e2a1b33eab24b084a661e9
Misha Behersky <bmwant@gmail.com>
2018-03-07 13:55:40 +0200

    Update license

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # !/usr/bin/env python
 
 from distutils.core import setup
+
+
 setup(
-    name='cookiecutter-pypackage',
+    name='cookiecutter-aiohttp-app',
     packages=[],
-    version='0.1.0',
-    description='Cookiecutter template for a Python package',
-    author='Audrey Roy Greenfeld',
-    license='BSD',
-    author_email='aroy@alum.mit.edu',
-    url='https://github.com/audreyr/cookiecutter-pypackage',
-    keywords=['cookiecutter', 'template', 'package', ],
+    version='0.1.1',
+    description='Cookiecutter template for a Python aiohttp web application',
+    author='Misha Behersky',
+    license='Unlicense',
+    author_email='bmwant@gmail.com',
+    url='https://github.com/bmwant/templio',
+    keywords=['cookiecutter', 'template', 'aiohttp', 'heroku'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -18,14 +20,10 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development',
     ],
 )
